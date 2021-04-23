e906a4320b571c82f8b228afc5a93b42e1a70f1d
Detik <the2ndflamingheart@gmail.com>
2021-04-23 20:07:36 +0700

    Init project

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  mode: 'jit',
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    safeList: [],
+    content: [
+      './index.html',
+      './src/**/*.jsx',
+      './src/**/*.js',
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
