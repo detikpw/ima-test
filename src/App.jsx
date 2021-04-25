5829184db8e0b8796e241f311b5e98c272784daf
Detik <the2ndflamingheart@gmail.com>
2021-04-26 06:51:58 +0700

    Refactor sidenav

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,58 @@
-import React, { useState } from 'react'
-import cart from './cart.svg'
-import search from './search.svg'
-import './App.css'
-import NavItem from './nav-item';
-import Right from './chevron-right.jsx';
-import Cube from './cube';
-import PortfolioItem from './portfolio-item';
-import Command from './command';
-import Feather from './feather';
-import FileText from './file-text';
-import LifeBouy from './life-bouy';
-import Message from './message';
-import Users from './users';
-import Video from './video';
+import React, { useState, useCallback } from "react";
+import cx from "classnames";
+import cart from "./cart.svg";
+import search from "./search.svg";
+import "./App.css";
+import NavItem from "./nav-item";
+import Right from "./chevron-right.jsx";
+import Cube from "./cube";
+import PortfolioItem from "./portfolio-item";
+import Command from "./command";
+import Feather from "./feather";
+import Menu from "./menu";
+import SideNav from "./side-nav";
 
 const portfolioItems = [
-  { icon: <Cube size={96} />,
-    label: 'Design',
-    description: 'A full stack all around designer that may or may not include a guide for specific creative people'
+  {
+    icon: <Cube size={96} />,
+    label: "Design",
+    description:
+      "A full stack all around designer that may or may not include a guide for specific creative people",
   },
-  { icon: <Command size={96} />,
-    label: 'Develop',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+  {
+    icon: <Command size={96} />,
+    label: "Develop",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
-  { icon: <Feather size={96} />,
-    label: 'Write',
-    description: 'Donec ullamcorper sollicitudin augue, sed venenatis nulla suscipit in.'
+  {
+    icon: <Feather size={96} />,
+    label: "Write",
+    description:
+      "Donec ullamcorper sollicitudin augue, sed venenatis nulla suscipit in.",
   },
-  { icon: <Cube size={96} />,
-    label: 'Promote',
-    description: 'Nunc scelerisque lacinia orci non ultrices. Maecenas molestie rhoncus felis, ut tincidunt odio pretium in'
-  }
-]
+  {
+    icon: <Cube size={96} />,
+    label: "Promote",
+    description:
+      "Nunc scelerisque lacinia orci non ultrices. Maecenas molestie rhoncus felis, ut tincidunt odio pretium in",
+  },
+];
 function App() {
-  const [count, setCount] = useState(0)
+  const [isMenuOpen, setMenu] = useState(false);
+
+  const toggleMenu = useCallback(() => setMenu(!isMenuOpen), [isMenuOpen]);
 
   return (
     <div className="App relative">
-      <aside className="hidden lg:block w-max h-0 ml-auto top-1/4 z-20 right-0 sticky">
-        <div className="rounded-l-lg shadow-md bg-white">
-          <div className="p-4 border-b-2">
-            <FileText />
-          </div>
-          <div className="p-4 border-b-2">
-            <LifeBouy />
-          </div>
-          <div className="p-4 border-b-2">
-            <Message />
-          </div>
-          <div className="p-4 border-b-2">
-            <Users />
-          </div>
-          <div className="p-4">
-            <Video />
-          </div>
-        </div>
-      </aside>
-      <header className="shadow sticky top-0 z-20 bg-white hidden lg:block">
+      <SideNav isSeen={isMenuOpen} />
+      <header className="shadow sticky top-0 z-20 bg-white lg:block">
         <div className="flex py-2">
-          <nav className="flex flex-grow">
+          <nav
+            className={cx("lg:flex flex-grow", {
+              hidden: !isMenuOpen,
+              flex: isMenuOpen,
+            })}
+          >
             <div className="flex items-center ml-auto font-bold">
               <NavItem>Demos</NavItem>
               <NavItem className="ml-2">Pages</NavItem>
@@ -66,11 +60,19 @@ function App() {
             </div>
           </nav>
           <div className="flex-grow" />
-          <div className="flex flex-grow">
-            <img src={cart} />
-            <img className="ml-4" src={search} />
-            <input className="ml-4 p-1 border border-black rounded-md" />
+          <div
+            className={cx("lg:flex flex-grow justify-end lg:justify-start", {
+              hidden: isMenuOpen,
+              flex: !isMenuOpen,
+            })}
+          >
+            <img src={search} />
+            <img className="ml-2" src={cart} />
+            <input className="hidden lg:block ml-4 p-1 border border-black rounded-md" />
           </div>
+          <button className="px-4 lg:hidden" onClick={toggleMenu}>
+            <Menu />
+          </button>
         </div>
       </header>
       <div className="mt-1 bg-white shadow-lg">
@@ -85,7 +87,8 @@ function App() {
                 We use only the finest and freshest ingredients
               </div>
               <div className="mt-2 lg:mt-8 lg:text-lg">
-                At Sway catering we know that food is an important part of life. If the meal is not perfect, your event cannot be perfect
+                At Sway catering we know that food is an important part of life.
+                If the meal is not perfect, your event cannot be perfect
               </div>
               <button className="mt-4 lg:mt-8 bg-green-500 py-2 px-4 rounded-full flex items-center mx-auto">
                 Request a quote
@@ -102,38 +105,56 @@ function App() {
             We specialize in corporate and private events
           </div>
           <p className="mt-8">
-            With 20 years of experience, we promise you the freshest, local ingredients,  hand-crafted cooking sprinkled with our unique whimsical elegance and exceptional service.
+            With 20 years of experience, we promise you the freshest, local
+            ingredients, hand-crafted cooking sprinkled with our unique
+            whimsical elegance and exceptional service.
           </p>
         </div>
       </div>
       <footer className="pt-4 mt-2 bg-white shadow-lg">
-        <div className="flex bg-alt-1 text-gray-400">
-          <div className="flex w-2/12 text-white text-lg bg-gray-700 items-center justify-center italic">
+        <div className="flex lg:flex-row flex-col bg-alt-1 text-gray-400">
+          <div className="flex lg:w-2/12 text-white text-lg bg-gray-700 items-center justify-center italic">
             My Portofolios
           </div>
-          <div className="flex flex-wrap w-6/12 px-8 content-start justify-center text-left">
-            {portfolioItems.map(item => (
+          <div className="flex flex-wrap lg:w-6/12 px-8 content-start justify-center text-left">
+            {portfolioItems.map((item) => (
               <PortfolioItem {...item} key={item.label} />
             ))}
           </div>
-          <div className="p-4 bg-alt-1 w-4/12 text-left">
-            <div className=" text-red-500">
-              Services
-            </div>
+          <div className="p-4 bg-alt-1 lg:w-4/12 text-left">
+            <div className=" text-red-500">Services</div>
             <p className="mt-6 text-sm">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
             </p>
             <p className="mt-6 text-xs">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
             </p>
             <p className="mt-6 text-xs">
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum."
             </p>
           </div>
         </div>
       </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/side-nav.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import FileText from './file-text';
+import LifeBouy from './life-bouy';
+import Message from './message';
+import Users from './users';
+import Video from './video';
+
+
+const sideNav = ({ isSeen }) => (
+  <aside className="lg:block w-max h-0 ml-auto top-9 lg:top-1/4 z-20 sticky">
+    <div className={cx('lg:right-0 rounded-l-lg shadow-md bg-white transition-all absolute lg:block', {'right-0': isSeen, '-right-16': !isSeen})}>
+      <div className="p-4 border-b-2">
+        <FileText />
+      </div>
+      <div className="p-4 border-b-2">
+        <LifeBouy />
+      </div>
+      <div className="p-4 border-b-2">
+        <Message />
+      </div>
+      <div className="p-4 border-b-2">
+        <Users />
+      </div>
+      <div className="p-4">
+        <Video />
+      </div>
+    </div>
+  </aside>
+);
+
+sideNav.propTypes = {
+  isSeen: PropTypes.bool,
+}
+
+export default sideNav;
